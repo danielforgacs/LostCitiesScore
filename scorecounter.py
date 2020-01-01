4b3542bd02a4714eee2df1e7bce3dba1fab97ce7
danielf@AcerS1 <-@->
2020-01-01 18:05:44 +0000

    works

--- scorecounter.py
+++ scorecounter.py
@@ -1,19 +1,53 @@
-scoretxt = 'ddd234567891'
-# scoretxt = 'd'
-# scoretxt = 'd1'
+scoretext = 'ddd234567891'
+scoretext = 'd'
+scoretext = 'd1'
+scoretext = '2'
+scoretext = '23'
+scoretext = 'd23'
+scoretext = 'ddd234681'
+scoretext = '281;281'
+scoretext = 'd281;dd281'
+scoretext = '291'
+scoretext = '291;291'
+scoretext = 'd291;291'
+scoretext = 'd291;d291'
 
-totalscrore = 0
-multiplier = 1
-
-for item in scoretxt:
-    if item == 'd':
-        multiplier += 1
-    elif item == '1':
-        totalscrore += 10
-    else:
-        totalscrore += int(item)
 
 
-totalscrore *= multiplier
 
-print(totalscrore)
+def count_colour(scoretext):
+    totalscrore = -20
+    multiplier = 1
+    count = 0
+
+    for count, item in enumerate(scoretext):
+        if item == 'd':
+            multiplier += 1
+        elif item == '1':
+            totalscrore += 10
+        else:
+            totalscrore += int(item)
+
+
+    totalscrore *= multiplier
+
+    if count > 7:
+        totalscrore += 20
+
+    return totalscrore
+
+
+
+
+
+
+if __name__ == '__main__':
+    scoretexts = scoretext.split(';')
+    print(scoretexts)
+
+    score = 0
+
+    for txt in scoretexts:
+        score += count_colour(scoretext=txt)
+
+    print(score)
