6c2d7317dce43007ec9f95180e6196408d671ef4
danielf@AcerS1 <-@->
2020-01-01 18:15:03 +0000

    started test

---
+++ scorecounter_test.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+ROOTDIR = (
+    os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+)))
+
+sys.path = [ROOTDIR] + sys.path
+
+import LostCitiesScore.scorecounter as counter
