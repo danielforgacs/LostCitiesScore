c5dc6e6cc0535df0896b729e5c57500a61e86a43
danielf@AcerS1 <-@->
2020-01-01 18:26:19 +0000

    added main module

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+ROOTDIR = (
+    os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+)))
+
+sys.path = [ROOTDIR] + sys.path
+
+import LostCitiesScore.scorecounter as counter
+
+
+
+if __name__ == '__main__':
+    scoretext = input('score: ')
+
+    print(counter.main(text=scoretext))
