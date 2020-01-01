0e0b1ace6bf1aed550cd8e0f65ada9f8d3b5a336
danielf@AcerS1 <-@->
2020-01-01 18:22:21 +0000

    wrote test

--- scorecounter_test.py
+++ scorecounter_test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pytest
 
 ROOTDIR = (
     os.path.dirname(
@@ -10,3 +11,13 @@ ROOTDIR = (
 sys.path = [ROOTDIR] + sys.path
 
 import LostCitiesScore.scorecounter as counter
+
+
+SCORE_TEXTS = [
+    ['281', 0],
+]
+
+
+@pytest.mark.parametrize('txt, expected', SCORE_TEXTS)
+def test_score_counter(txt, expected):
+    assert counter.main(text=txt) == expected
