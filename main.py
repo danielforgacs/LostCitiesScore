fabac1237763affda90dd3da65bad3953e1b80f3
danielf@AcerS1 <-@->
2020-01-03 20:45:13 +0000

    wip

--- main.py
+++ main.py
@@ -14,12 +14,41 @@ import LostCitiesScore.scorecounter as counter
 
 
 if __name__ == '__main__':
+    # rounds = ['p1-r1', 'p2-r1']
 
-    rounds = ['p1-r1', 'p2-r1']
+    # for round_ in rounds:
+    #     print(round_)
+    #     scoretext = input('score: ')
+    #     # print(counter.main(text=scoretext))
 
-    for round_ in rounds:
-        print(round_)
-        scoretext = input('score: ')
-        # print(counter.main(text=scoretext))
+    #     # scoretext = input('score: ')
 
-        # scoretext = input('score: ')
+    p1score = 0
+    p2score = 0
+
+    scoretext = input('Player 1 cards: ')
+    p1score += counter.main(text=scoretext)
+
+    scoretext = input('\nPlayer 2 cards: ')
+    p2score += counter.main(text=scoretext)
+
+    print('.'*79)
+
+    scoretext = input('Player 1 cards: ')
+    p1score += counter.main(text=scoretext)
+
+    scoretext = input('\nPlayer 2 cards: ')
+    p2score += counter.main(text=scoretext)
+
+    print('.'*79)
+
+    scoretext = input('Player 1 cards: ')
+    p1score += counter.main(text=scoretext)
+
+    scoretext = input('\nPlayer 2 cards: ')
+    p2score += counter.main(text=scoretext)
+
+    print('-'*79)
+
+    print('Player 1 score total: {}'.format(p1score))
+    print('Player 2 score total: {}'.format(p2score))
