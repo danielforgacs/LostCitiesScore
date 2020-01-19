e5a19647ad3854775b538f9d4a75d19944e4fb0b
danielf@AcerS1 <-@->
2020-01-19 16:21:38 +0000

    added docks

--- LostCitiesScore/lostcitiesscore/forms.py
+++ LostCitiesScore/lostcitiesscore/forms.py
@@ -3,6 +3,10 @@ from django import forms
 
 
 class IndexForm(forms.Form):
+    """
+    For starters, a reduntant
+    simple form with both player all rounds.
+    """
     cards_A_round_1 = forms.CharField()
     cards_B_round_1 = forms.CharField()
 
