8a8682575627a6fb67ddcb3dcf859e56a65bfaec
danielf@AcerS1 <-@->
2020-01-19 16:19:17 +0000

    very basic redundant form

--- LostCitiesScore/lostcitiesscore/forms.py
+++ LostCitiesScore/lostcitiesscore/forms.py
@@ -3,5 +3,11 @@ from django import forms
 
 
 class IndexForm(forms.Form):
-    cards_A = forms.CharField()
-    cards_B = forms.CharField()
+    cards_A_round_1 = forms.CharField()
+    cards_B_round_1 = forms.CharField()
+
+    cards_A_round_2 = forms.CharField()
+    cards_B_round_2 = forms.CharField()
+
+    cards_A_round_3 = forms.CharField()
+    cards_B_round_3 = forms.CharField()
