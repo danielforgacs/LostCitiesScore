22dd0d713eb99c787044704158030cc794ab0213
danielf@AcerS1 <-@->
2020-01-19 18:27:54 +0000

    __wip__; getting POST data

--- LostCitiesScore/lostcitiesscore/views.py
+++ LostCitiesScore/lostcitiesscore/views.py
@@ -3,6 +3,10 @@ from . import forms
 
 def index(request):
     form = forms.IndexForm()
+
+    if request.method == 'POST':
+        request.POST.get('cards_A_round_1')
+
     result = render(
         request=request,
         template_name='lostcitiesscore/index.html',
