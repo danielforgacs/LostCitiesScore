f3d3f6bb062412830d418cef3a16de80b4b44057
danielf@AcerS1 <-@->
2020-01-20 00:48:38 +0000

    __wip__

--- LostCitiesScore/lostcitiesscore/views.py
+++ LostCitiesScore/lostcitiesscore/views.py
@@ -5,7 +5,14 @@ def index(request):
     form = forms.IndexForm()
 
     if request.method == 'POST':
-        request.POST.get('cards_A_round_1')
+        cards_A1 = request.POST.get('cards_A_round_1')
+        cards_B1 = request.POST.get('cards_B_round_1')
+
+        cards_A2 = request.POST.get('cards_A_round_2')
+        cards_B2 = request.POST.get('cards_B_round_2')
+
+        cards_A3 = request.POST.get('cards_A_round_3')
+        cards_B3 = request.POST.get('cards_B_round_3')
 
     result = render(
         request=request,
