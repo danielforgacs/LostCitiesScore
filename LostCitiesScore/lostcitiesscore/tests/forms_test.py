bc6000bfca01e3ab4b4a7ddde1c4d5e143fcdc1d
danielf@BurrowsVMUbu19.10 <daniel.forgacs@weareburrows.com>
2020-01-20 10:57:54 +0000

    wrote test; ;

--- LostCitiesScore/lostcitiesscore/tests/forms_test.py
+++ LostCitiesScore/lostcitiesscore/tests/forms_test.py
@@ -7,3 +7,19 @@ def test_can_init_form():
     form = forms.IndexForm()
 
     assert isinstance(form, Form)
+    assert not form.is_bound
+
+
+
+def test_accepts_player_round_data():
+    data = {
+        'p1r1': 'a',
+        'p2r1': 'a',
+        'p1r2': 'a',
+        'p2r2': 'a',
+        'p1r3': 'a',
+        'p2r3': 'a',
+    }
+    form = forms.IndexForm(data=data)
+
+    assert form.is_bound
