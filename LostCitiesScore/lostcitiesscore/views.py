5126cb1f9278d37e410859c1fb1cbbc7a2b79581
danielf@AcerS1 <-@->
2020-01-21 18:01:43 +0000

    fixed initializing form; todo: validation error response

--- LostCitiesScore/lostcitiesscore/views.py
+++ LostCitiesScore/lostcitiesscore/views.py
@@ -2,17 +2,11 @@ from django.shortcuts import render
 from . import forms
 
 def index(request):
-    form = forms.IndexForm()
+    form = forms.IndexForm(data=request.POST.dict() or None)
 
     if request.method == 'POST':
-        cards_A1 = request.POST.get('cards_A_round_1')
-        cards_B1 = request.POST.get('cards_B_round_1')
-
-        cards_A2 = request.POST.get('cards_A_round_2')
-        cards_B2 = request.POST.get('cards_B_round_2')
-
-        cards_A3 = request.POST.get('cards_A_round_3')
-        cards_B3 = request.POST.get('cards_B_round_3')
+        if form.is_valid:
+            print('YAAAAAAY')
 
     result = render(
         request=request,
