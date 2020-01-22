5a193a45fc3728a7878e8fbbf677ac96f0720734
danielf@BurrowsVMUbu19.10 <daniel.forgacs@weareburrows.com>
2020-01-22 13:31:15 +0000

    started testing the views

---
+++ LostCitiesScore/lostcitiesscore/tests/views_test.py
@@ -0,0 +1,12 @@
+from lostcitiesscore import views
+from django.http import HttpRequest
+from django.http import QueryDict
+
+
+def test_alkwsjhf():
+    data = QueryDict('a=1&a=2&c=3')
+    request = HttpRequest()
+    request.method = 'POST'
+
+
+    print(dir(request))
