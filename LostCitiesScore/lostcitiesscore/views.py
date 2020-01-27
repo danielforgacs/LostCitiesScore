ca781f069adc5c90a9e1e3e6fb6d6019d3a3bc73
danielf@AcerS1 <-@->
2020-01-27 16:18:35 +0000

    working on final data

--- LostCitiesScore/lostcitiesscore/views.py
+++ LostCitiesScore/lostcitiesscore/views.py
@@ -13,13 +13,55 @@ def index(request):
 
     if form.is_valid():
         template = 'lostcitiesscore/scores.html'
+        pl1rnd1 = form.data['cards_A_round_1']
+        pl2rnd1 = form.data['cards_B_round_1']
+        pl1rnd2 = form.data['cards_A_round_2']
+        pl2rnd2 = form.data['cards_B_round_2']
+        pl1rnd3 = form.data['cards_A_round_3']
+        pl2rnd3 = form.data['cards_B_round_3']
+
+        context['data'] = {
+            1: {
+                1: {
+                    'cards': pl1rnd1,
+                },
+
+                2: {
+                    'cards': pl2rnd1,
+                },
+            },
+
+
+            2: {
+                1: {
+                    'cards': pl1rnd1,
+                },
+
+                2: {
+                    'cards': pl2rnd1,
+                },
+            },
+
+
+            3: {
+                1: {
+                    'cards': pl1rnd1,
+                },
+
+                2: {
+                    'cards': pl2rnd1,
+                },
+            },
+        }
+
+
         context['scores'] = [
-            form.data['cards_A_round_1'],
-            form.data['cards_B_round_1'],
-            form.data['cards_A_round_2'],
-            form.data['cards_B_round_2'],
-            form.data['cards_A_round_3'],
-            form.data['cards_B_round_3'],
+            scnt.count_colour(scoretext=form.data['cards_A_round_1']),
+            scnt.count_colour(scoretext=form.data['cards_B_round_1']),
+            scnt.count_colour(scoretext=form.data['cards_A_round_2']),
+            scnt.count_colour(scoretext=form.data['cards_B_round_2']),
+            scnt.count_colour(scoretext=form.data['cards_A_round_3']),
+            scnt.count_colour(scoretext=form.data['cards_B_round_3']),
         ]
 
         # pl1_r1 = form.data['cards_A_round_1']
