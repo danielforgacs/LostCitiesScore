5f815e0ac20d83ef87e52e1f0f3e7e7cffaec4b9
danielf@AcerS1 <-@->
2020-02-01 18:24:50 +0000

    fixed docker test runner

--- dockertest.sh
+++ dockertest.sh
@@ -1,1 +1,3 @@
+docker run --rm -d -v $PWD:/home -e DEBUG=1 -p 8000:8000  --name testrun lostcitiesscore
 docker run --rm -v $PWD:/home -e DEBUG=1 --network="host" lostcitiesscore pytest LostCitiesScore/
+docker kill testrun
